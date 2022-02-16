fee2d9db839ab062604fdf0e850a4c1da01f9aa3
vrutti lad <vruttilad2000@gmail.com>
2022-02-16 17:13:58 +0530

    Add project files.

---
+++ openbrowserss/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Drawing;
+using Xamarin.Essentials;
+
+namespace openbrowserss
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button button;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            UIReferences();
+            UIClickEvents();
+        }
+
+        private void UIClickEvents()
+        {
+            button.Click += Button_Click;
+        }
+
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync(uri: "https://www.google.com/", new BrowserLaunchOptions
+            {
+                LaunchMode = BrowserLaunchMode.SystemPreferred,
+                TitleMode = BrowserTitleMode.Show,
+                PreferredToolbarColor = Color.Red,
+
+
+            });
+        }
+
+        private void UIReferences()
+        {
+            button = FindViewById<Button>(Resource.Id.button1);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
